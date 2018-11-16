671933df1d30c13df34efc1a78a6710e52e5ae26
kimseongjung <plznw4me@naver.com>
2018-11-16 16:23:38 +0900

    first commit lesson 1

--- RNN_stock.py
+++ RNN_stock.py
@@ -40,8 +40,8 @@ def MinMaxScaler(data):
 # train Parameters
 seq_length = 7
 data_dim = 5
-hidden_dim = 10
-output_dim = 1
+hidden_size = 10
+output_size = 1
 learning_rate = 0.01
 iterations = 500
 
