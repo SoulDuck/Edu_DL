1bd32d7e16b122698270f4fb3a36c1c883bbd608
kimseongjung <plznw4me@naver.com>
2018-12-03 23:32:49 +0900

    first commit lesson 1

--- lesson4/keras_resnet.py
+++ lesson4/keras_resnet.py
@@ -1,36 +1,193 @@
 import keras
 from keras.models import Model
 from keras.layers.merge import add
-from keras.layer import Input , Activation , Dense, Flatten , Conv2D
-
+from keras.layer import Input , Activation , Dense, Flatten , Conv2D ,MaxPooling2D
+import configure as cfg
+"""
+ref : 
+"""
 def stem(input):
+    layer = Conv2D(64, kernel_size=(7,7), strides=(2,2), padding='same', activation='relu')(input)
+    layer = MaxPooling2D(kernel_size=(3,3) , strides=(2,2))(layer)
+    return layer
 
-def residual_block(input , kernel_sizes , out_chs , strides):
-    layer = input
-    ksizes_outchs_strides = zip(kernel_sizes, out_chs, strides)
+def residual_block(input, out_ch, kernel_size=(3,3), strides=(1,1)):
+    """
 
-    for k, ch, s in ksizes_outchs_strides:
-        layer = Conv2D(ch, (k, k), strides=(s, s), padding='same', activation='relu')(layer)
+    :param input:
+    :param out_ch:
+    :param kernel_size:
+    :param strides:
+    :return:
+    """
 
+    layer = Conv2D(out_ch, kernel_size, strides=strides, padding='same', activation='relu')(input)
+    layer = Conv2D(out_ch, kernel_size, strides=strides, padding='same', activation='relu')(layer)
     return add(input , layer )
 
-def residual_block_projection(input , kernel_sizes , out_chs , strides):
+def residual_block_projection(input, out_ch, kernel_size=(3,3), strides=(1,1)):
     """
-    Feature Map Size 가 작아지는
+    Feature map 의 size 가 줄어들면 Channel 이 늘어납니다.
+    이때 shortcut 의 Channel 의 갯수를 맞추어 주기 위해 Projection Block 을 사용합니다
     :param input:
     :param kernel_sizes:
     :param out_chs:
     :param strides:
     :return:
     """
-    layer = input
-    projection_input = Conv2D(out_chs[-1], (1, 1), strides=(1, 1), padding='same', activation='relu')(layer)
-    ksizes_outchs_strides = zip(kernel_sizes, out_chs, strides)
-    for k, ch, s in ksizes_outchs_strides:
-        layer = Conv2D(ch, (k, k), strides=(s, s), padding='same', activation='relu')(layer)
+    projection_input  = Conv2D(out_ch, (1,1), strides=strides, padding='same', activation='relu')(input)
+    layer = Conv2D(out_ch, kernel_size, strides=strides, padding='same', activation='relu')(input)
+    layer = Conv2D(out_ch, kernel_size, strides=strides, padding='same', activation='relu')(layer)
+    return add(projection_input , layer)
+
+def bottlenect_block(input, out_ch, kernel_size=(3,3), strides=(1,1)):
+    """
+
+    :param input:
+    :param out_ch:
+    :param kernel_size:
+    :param strides:
+    :return:
+    """
+    layer = Conv2D(out_ch, (1,1), strides=strides, padding='same', activation='relu')(input)
+    layer = Conv2D(out_ch, kernel_size, strides=strides, padding='same', activation='relu')(layer)
+    layer = Conv2D(out_ch * 4 , (1,1), strides=strides, padding='same', activation='relu')(layer)
+    return add(input , layer)
+
+def bottlenect_block_projection(input, out_ch, kernel_size=(3,3), strides=(1,1)):
+    """
+
+    :param input:
+    :param out_ch:
+    :param kernel_size:
+    :param strides:
+    :return:
+    """
+    projection_input = Conv2D(out_ch, (1, 1), strides=strides, padding='same', activation='relu')(input)
+
+    layer = Conv2D(out_ch, (1,1), strides=strides, padding='same', activation='relu')(input)
+    layer = Conv2D(out_ch, kernel_size, strides=strides, padding='same', activation='relu')(layer)
+    layer = Conv2D(out_ch * 4 , (1,1), strides=strides, padding='same', activation='relu')(layer)
     return add(projection_input , layer)
 
 
-def bottlenect_block(input , ksizes , out_chs , strides):
+def resnet18(input):
+    layer=stem(input)
+    # Block A
+    layer = residual_block(layer, 64)
+    layer = residual_block(layer, 64)
 
-def bottlenect_block_projection(input, ksizes, out_chs, strides):
+    # Block B
+    layer = residual_block_projection(layer, 128)
+    layer = residual_block(layer, 128)
+
+    # Block C
+    layer = residual_block_projection(layer, 256)
+    layer = residual_block(layer, 256)
+
+    # Block D
+    layer = residual_block_projection(layer, 512)
+    layer = residual_block(layer, 512)
+
+    return layer
+
+def resnet_34(input):
+    layer = stem(input)
+    # Block A
+    layer = residual_block(layer, 64)
+    layer = residual_block(layer, 64)
+    layer = residual_block(layer, 64)
+
+    # Block B
+    layer = residual_block_projection(layer, 128)
+    layer = residual_block(layer, 128)
+    layer = residual_block(layer, 128)
+    layer = residual_block(layer, 128)
+
+    # Block C
+    layer = residual_block_projection(layer, 256)
+    layer = residual_block(layer, 256)
+    layer = residual_block(layer, 256)
+    layer = residual_block(layer, 256)
+    layer = residual_block(layer, 256)
+    layer = residual_block(layer, 256)
+
+
+    # Block D
+    layer = residual_block_projection(layer, 512)
+    layer = residual_block(layer, 512)
+    layer = residual_block(layer, 512)
+
+    return layer
+
+
+# Resnet 50
+def resnet_50(input):
+    layer = stem(input)
+    # Block A
+    bottlenect_block(layer, out_ch=64, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=64, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=64, kernel_size=(3, 3))
+    # Block B
+    bottlenect_block_projection(layer, out_ch=128, kernel_size=(3,3))
+    bottlenect_block(layer, out_ch=128, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=128, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=128, kernel_size=(3, 3))
+    # Block C
+    bottlenect_block_projection(layer, out_ch=256, kernel_size=(3,3))
+    bottlenect_block(layer, out_ch=256, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=256, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=256, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=256, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=256, kernel_size=(3, 3))
+    # Block D
+    bottlenect_block_projection(layer, out_ch=512, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=512, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=512, kernel_size=(3, 3))
+
+
+
+# Resnet 101
+def resnet_101(input):
+    layer = stem(input)
+    # Block A
+    bottlenect_block(layer, out_ch=64, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=64, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=64, kernel_size=(3, 3))
+    # Block B
+    bottlenect_block_projection(layer, out_ch=128, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=128, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=128, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=128, kernel_size=(3, 3))
+    # Block C
+    bottlenect_block_projection(layer, out_ch=256, kernel_size=(3, 3))
+    for i in range(22):
+        layer = bottlenect_block(layer, out_ch=256, kernel_size=(3, 3))
+    # Block D
+    bottlenect_block_projection(layer, out_ch=512, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=512, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=512, kernel_size=(3, 3))
+
+def resnet_152(input):
+    layer = stem(input)
+
+    # Block A
+    bottlenect_block(layer, out_ch=64, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=64, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=64, kernel_size=(3, 3))
+
+    # Block B
+    bottlenect_block_projection(layer, out_ch=128, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=128, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=128, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=128, kernel_size=(3, 3))
+
+    # Block C
+    bottlenect_block_projection(layer, out_ch=256, kernel_size=(3, 3))
+    for i in range(35):
+        layer = bottlenect_block(layer, out_ch=256, kernel_size=(3, 3))
+
+    # Block D
+    bottlenect_block_projection(layer, out_ch=512, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=512, kernel_size=(3, 3))
+    bottlenect_block(layer, out_ch=512, kernel_size=(3, 3))
