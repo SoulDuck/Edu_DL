b79da7fd4bdb0bfada87a70c4809d6bfd838e973
sangjaekang <sangjaekang48@gmail.com>
2018-12-03 15:37:08 +0900

    refactor: DogExtractor 코드를 pandas Style Indexing으로 변경

--- dog_breed.py
+++ dog_breed.py
@@ -1,244 +1,208 @@
 import os
-import zipfile
 import tarfile
-import csv
-from utils import paths2np, cls2onehot
+import pandas as pd
 import numpy as np
 import sys
-from utils import get_extension
 from urllib import request
-import random
+from tqdm import tqdm
+import glob
+import cv2
+import warnings
+from functools import lru_cache
+
+def download_dog_bread_dataset(download_dir):
+    """
+    stanford에서 제공하는 dog bread classification dataset을 다운
+
+    :param download_dir: 저장할 디렉토리
+    :return:
+    """
+    DOG_BREAD_DATASET_URL = "http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar"
+    # 데이터 받아오기(download & unzip)
+    zip_path = download_data_url(DOG_BREAD_DATASET_URL, download_dir)
+    image_dir = os.path.join(download_dir, "Images/")
+    if not os.path.exists(image_dir):
+        print("Extract Zip Folder to {}".format(image_dir))
+        with tarfile.open(name=zip_path, mode='r') as zipfile:
+            zipfile.extractall(download_dir)
+    else:
+        print("Already extracted")
+
+    # summarize data and write datainfo
+    info_path = os.path.join(download_dir, "datainfo.txt")
+    if not os.path.exists(info_path):
+        print("Summarize DataSet")
+        info_df = summarize_directory(image_dir)
+        info_df.to_csv(info_path,index=True)
+    else:
+        print("Already Summarized")
 
 
-class Extracter(object):
-    def __init__(self):
-        pass
+def download_data_url(url, download_dir):
+    """
+    url를 통해 데이터를 download_dir에 저장
 
-    def generate_datainfo(self, datainfo_path, image_dir, n_limit=None):
+    :param url: 가져올 주소
+    :param download_dir: 저장할 디렉토리
+    :return:
+    """
+    filename = os.path.split(url)[1]
+    file_path = os.path.join(download_dir, filename)
+    if not os.path.exists(file_path):
+        print("Download {} to {}".format(url, file_path))
+        os.makedirs(download_dir, exist_ok=True)
+
+        def download_reporthook(count, block_size, total_size):
+            """
+            다운로드 진행 경과를 report하는 hook
+            """
+            progresses = float(count * block_size) / total_size
+            msg = "\r {:2.3f}% already downloaded".format(progresses * 100)
+            sys.stdout.write(msg)
+            sys.stdout.flush()
+
+        file_path, _ = request.urlretrieve(url=url, filename=file_path,
+                                           reporthook=download_reporthook)
+    else:
+        print('data is already downloaded')
+    return file_path
+
+
+def summarize_directory(dataset_dir):
+    """
+    데이터 셋이 저장된 디렉토리의 이미지와 라벨을 summarize한 dataframe을 retrun
+    dataset_dir
+        |- label_1
+            |- image1
+            |- image2
+        |- label_2
+            |- image3
+            |- image4
+        |- label_3
+            |- image5
+
+    output : pd.DataFrame
+    | file_path | label |
+    | -----     | ----  |
+    | dataset_dir/label_1/image1 | label_1 |
+    | dataset_dir/label_1/image2 | label_1 |
+    | dataset_dir/label_2/image3 | label_2 |
+    | dataset_dir/label_2/image4 | label_2 |
+    | dataset_dir/label_3/image5 | label_3 |
+
+    :param dataset_dir: 저장된 데이터셋의 디렉토리(데이터셋의 root directory)
+    :return:
+    """
+    info_df = pd.DataFrame(columns=['file_path', 'label'])
+    for file_path in tqdm(glob.glob(os.path.join(dataset_dir, "*/*"))):
+        label_name = file_path.split(os.sep)[-2]
+        absolute_path = os.path.abspath(file_path)
+        info_df = info_df.append({
+            "file_path" : absolute_path,
+            "label" : label_name
+        },ignore_index=True)
+    return info_df
+
+
+class DogExtractor(object):
+    """
+    스탠포드 데이터셋 dog bread classification의 데이터를 extract하는 메소드
+
+    Example
+    >>> dex = DogExtractor("./data")
+    >>> image, label = dex[0] # indexing by integer
+    >>> images, labels = dex[0:10] # indexing by range
+    >>> images, labels = dex[[1,3,5,9]] # indexing by list
+    >>> dex.index[:10]
+    Int64Index([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype='int64')
+    >>> dex.labels[0:1000:200]
+    0                n02097658-silky_terrier
+    200         n02092002-Scottish_deerhound
+    400         n02092002-Scottish_deerhound
+    600               n02091244-Ibizan_hound
+    800    n02095314-wire-haired_fox_terrier
+    Name: label, dtype: object
+
+    """
+    def __init__(self, data_dir, image_shape=(224, 224, 3), keep_aspect_ratio=True):
         """
-        TODO : Dataframe 을 이용하는 것으로 바꾸기
-        이 함수는 폴더 구성이 아래와 같음을 예상하고 만들었음
-        images
-            |- dog1
-                |-1.jpg
-                |-2.jpg
-                ...
-            |- dog2
-                |-1.jpg
-                |-2.jpg
-                ...
 
-        filepath , labelname , label(number) 가 저장됩니다.
-        Mockup 파일이 아니면 limit_count 을 특정 숫자로 지정해 주면 됩니다.
-
-        :return:
+        :param data_dir: dog bread classification이 담겨 있는 directory, 없으면 이 경로로 다운받음
+        :param image_shape: 출력받을 이미지의 크기
+        :param keep_aspect_ratio: 이미지의 비율을 고정시키는가 유무 (True이면, padding을 넣어서 image_shape로 resize함)
         """
-        # Generate csv file
-        f = open(datainfo_path, 'w')
-        writer = csv.DictWriter(f, fieldnames=["filepath", "labelname", "label" ])
-        # write CSV Head
-        writer.writerow({"filepath": 'filepath', "labelname": "labelname", "label": "label"})
-        label_count = 0
+        self.image_shape = image_shape
+        self.keep_aspect_ratio = keep_aspect_ratio
 
-        # Crawling target dir
-        for i, (rt_dir, subdir, files) in enumerate(os.walk(image_dir)):
-            if i == 0:
-                # i==0 은 시작한 자기 폴더라서 뛰어넘어버립니다.
-                pass;
+        info_path = os.path.join(data_dir, "datainfo.txt")
+        if not os.path.exists(info_path):
+            download_dog_bread_dataset(data_dir)
+        self.info_df = pd.read_csv(info_path, index_col=0)
+
+    def __len__(self):
+        return len(self.info_df)
+
+    @property
+    def index(self):
+        return self.info_df.index
+
+    @property
+    def labels(self):
+        return self.info_df.label
+
+    def __getitem__(self, slc):
+        if isinstance(slc, int):
+            return self._view_by_index(slc)
+        elif isinstance(slc, slice):
+            return self._view_by_dataframe(self.info_df.loc[slc])
+        elif isinstance(slc, list):
+            return self._view_by_dataframe(self.info_df.loc[slc])
+        elif isinstance(slc, tuple):
+            return self._view_by_dataframe(self.info_df.loc[list(slc)])
+        else:
+            raise ValueError("적절치 못한 indexing입니다.")
+
+    @property
+    def label_counts(self):
+        return self.info_df.label.value_counts()
+
+    @lru_cache(maxsize=30000)
+    def _view_by_index(self, index):
+        row = self.info_df.loc[index]
+        image = self._read_image(row.file_path)
+        label = row.label
+        return image, label
+
+    def _view_by_dataframe(self, view_df):
+        images = []
+        labels = []
+        for index, _ in view_df.iterrows():
+            image, label = self._view_by_index(index)
+            images.append(image)
+            labels.append(label)
+        return np.stack(images), labels
+
+    def _read_image(self, file_path):
+        image = cv2.imread(file_path)
+        try:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        except:
+            warnings.warn("{} 파일을 읽을 수 없습니다.".format(file_path))
+            return np.zeros(self.image_shape)
+
+        if self.keep_aspect_ratio:
+            dst_h, dst_w = self.image_shape[:2]
+            src_h, src_w = image.shape[:2]
+            h_ratio, w_ratio = dst_h / src_h, dst_w / src_w
+            if h_ratio < w_ratio:
+                image = cv2.resize(image, None, fx=h_ratio, fy=h_ratio)
+                delta = self.image_shape[1] - image.shape[1]
+                image = cv2.copyMakeBorder(image, 0, 0, delta // 2, delta - delta // 2,
+                                           cv2.BORDER_CONSTANT, value=[0, 0, 0])
             else:
-                labelname = os.path.split(rt_dir)[-1]
-                for count,file in enumerate(files[:n_limit]):
-                    file_ext = get_extension(file)
-                    # Checking image files
-                    if file_ext in ['.jpg', 'jpeg', 'png', 'JPG', 'JPEG']:
-                        filepath = os.path.join(rt_dir, file)
-                        # write path , labelname , label to csv file
-                        writer.writerow({"filepath": filepath, "labelname": labelname, "label": label_count})
-                print('{} : {} '.format(rt_dir, label_count))
-                label_count += 1
-        f.close()
-
-    def get_images_labels(self, onehot, n_classes):
-        raise NotImplementedError
-
-    @classmethod
-    def get_col_elements(cls, csv_path, col_idx):
-        """
-        csv파일을 불러옵니다.
-        특정 columne 의 모든 elements 을 가져옵니다.
-        :param fpath:
-        :param col_idx:
-        :return:
-        """
-        f = open(csv_path, 'r')
-        lines=f.readlines()
-        ret_paths = []
-        for line in lines[1:]:
-            if len(line.split(',')) ==0:
-                continue
-            ret_paths.append(line.split(',')[col_idx])
-        return ret_paths
-
-    @classmethod
-    def report_download_progress(cls, count, block_size, total_size):
-        pct_complete = float(count * block_size) / total_size
-        msg = "\r {0:1%} already downloaded".format(pct_complete)
-        sys.stdout.write(msg)
-        sys.stdout.flush()
-
-    @classmethod
-    def download_data_url(cls, url, download_dir):
-        filename = url.split('/')[-1]
-        file_path = os.path.join(download_dir, filename)
-        if not os.path.exists(file_path):
-            try:
-                os.makedirs(download_dir)
-            except Exception:
-                pass
-            print("Download %s  to %s" % (url, file_path))
-            file_path, _ = request.urlretrieve(url=url, filename=file_path, reporthook=cls.report_download_progress)
-        else:
-            print('cifar is already downloaded')
-        return file_path
-
-    @classmethod
-    def unzip(cls, file_path, extract_dir):
-        print('\nExtracting files')
-        if file_path.endswith(".zip"):
-            zipfile.ZipFile(file=file_path, mode="r").extracall(extract_dir)
-        elif file_path.endswith(".tar.gz"):
-            tarfile.open(name=file_path, mode='r:gz').extractall(extract_dir)
-        elif file_path.endswith(".tgz"):
-            tarfile.open(name=file_path, mode='r:gz').extractall(extract_dir)
-        elif file_path.endswith(".tar"):
-            tarfile.open(name=file_path, mode='r').extractall(extract_dir)
-
-
-    @classmethod
-    def divide_train_val(self, np_images , labels , validation_ratio , n_classes):
-        """
-        비율에 따라 데이터셋을 나눕니다.
-        전체 데이터 셋에 대해서 나누는 것이 아니라 class 별 데이터셋에 대해 지정된 ratio 비율로 나눕니다.
-
-        Ex)
-        validation_ratio = 0.2 , train ratio = 0.8
-        n class1 = 100 | n class2 = 10
-
-        class1 : n train = 80 | n validation = 20
-        class2 : n train = 8  | n validation= 2
-
-        :param images:
-        :param labels:
-        :param validation_ratio:
-        :param n_classes:
-        :return:
-        """
-        # if labels is OneHot Vector
-        if np.ndim(labels)==2:
-            labels = np.argmax(labels , axis=1)
-
-        np_images = np.asarray(np_images)
-        train_imgs = []
-        train_labs = []
-        val_imgs = []
-        val_labs = []
-
-        for class_idx in range(n_classes):
-            target_indices = np.where([labels == class_idx])[1]
-            n_target = len(target_indices)
-            n_val = int(n_target * validation_ratio)
-            n_train = int(n_target - n_val)
-            print('class index : {} \t # train : {} \t # validation :{}'.format(class_idx , n_train , n_val))
-
-            # Extract indices
-            random.shuffle(target_indices)
-            train_indices = target_indices[:n_train]
-            val_indices = target_indices[n_train:]
-
-            # Stack images and labels
-            train_imgs.append(np_images[train_indices])
-            train_labs.extend(labels[train_indices])
-            val_imgs.append(np_images[val_indices])
-            val_labs.extend(labels[val_indices])
-
-        # list 2 Numpy
-        train_imgs = np.vstack(train_imgs)
-        val_imgs = np.vstack(val_imgs)
-
-        return train_imgs , train_labs , val_imgs , val_labs
-
-class Transformer(object):
-    def __init__(self):
-        raise NotImplementedError
-
-
-
-class Dog_Extractor(Extracter):
-    def __init__(self , image_dir , sample_per_limit = None):
-        url='http://vision.stanford.edu/aditya86/ImageNetDogs/images.tar'
-        super(Dog_Extractor , self).__init__()
-        self.sample_per_limit = sample_per_limit
-        # if dataset was downloaded or the folder exists in `image_dir`
-        # download is not performed
-        if not os.path.isdir(image_dir):
-            fpath = self.download_data_url(url, image_dir)
-            # image dir : dir path to Unzip
-            self.unzip(fpath , image_dir)
-
-        self.image_dir = os.path.join(image_dir , 'Images')
-        # record [filepath , label] to csv file
-        self.datainfo_path = 'dataset_info.csv'
-
-        if not self.sample_per_limit is None:
-            # Mock-Up
-            self.generate_datainfo(self.datainfo_path, self.image_dir, self.sample_per_limit)
-            self.imgs, self.labs = self.get_images_labels(True, n_classes=120)
-
-        else:
-            # Real Data
-            self.generate_datainfo(self.datainfo_path, self.image_dir)
-            self.imgs, self.labs = self.get_images_labels(True, n_classes=120)
-        #
-
-
-    def get_images_labels(self, onehot, n_classes):
-        """
-        self.datainfo_path
-        :param onehot:
-        :param n_classes:
-        :return:
-
-        이렇게 코드를 짜면 문제가 있다.
-        image 에 문제가 있을면 label에는 반영되지 않는다.
-        """
-        paths = self .get_col_elements(self.datainfo_path, 0)
-        labels = self.get_col_elements(self.datainfo_path, 2)
-        images , err_indices = paths2np(paths, (255, 255))
-
-        # 에러가 난 index 을 labels 이 담겨있는 list에도 빼주어야 한다.
-        # list 에서 element 을 추출하면 pop 을 하는동안 list 형태가 바뀌어서 거꾸로 빼줘야 한다.
-        #labels = map(labels.pop , err_indices)
-        err_indices.reverse()
-        for idx in err_indices:
-            labels.pop(idx)
-        # str ==> int
-        labels = list(map(int , labels))
-        if onehot:
-            labels = cls2onehot(labels, n_classes)
-        return np.asarray(images) , np.asarray(labels)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                image = cv2.resize(image, None, fx=h_ratio, fy=w_ratio)
+                delta = self.image_shape[0] - image.shape[0]
+                image = cv2.copyMakeBorder(image, delta // 2, delta - delta // 2, 0, 0,
+                                           cv2.BORDER_CONSTANT, value=[0, 0, 0])
+        dst_h, dst_w = self.image_shape[:2]
+        return cv2.resize(image, (dst_w, dst_h))
