3cbdc70130dd18c0f576e68776ae8f2c040f44e4
sangjaekang <sangjaekang48@gmail.com>
2018-12-10 18:31:28 +0900

    fix: doggenerator의 next_batch가 1epoch에 모든 데이터셋 read 보장

--- load.py
+++ load.py
@@ -1,20 +1,20 @@
-from keras.utils import Sequence
 import numpy as np
-import random
 
 
-class DogDataGenerator(Sequence):
+class DogDataGenerator:
     """
     StanFord Dog Breed DataGenerator
     reference : https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
     """
+
     def __init__(self, extractor, extractor_index=None, pipeline=None,
-                 batch_size=32, shuffle=True):
+                 onehot=True, batch_size=32, shuffle=True):
         """
 
         :param extractor: extractor.DogExtractor
         :param extractor_index: extractor에서 가져올 해당 index, None이면, 전체 index
         :param pipeline: 적용할 데이터 전처리 파이프라인 (transform.py에 구현된 파이프라인)
+        :param onehot: One-Hot Vector로 출력할 것인지, code로 출력할 것인지 결정
         :param batch_size: 배치 크기
         :param shuffle: 순서를 섞을 것인지 유무
         """
@@ -27,7 +27,12 @@ class DogDataGenerator(Sequence):
         self.indexes = np.arange(len(self.extractor_indexes))
 
         self.pipeline = pipeline
+        self.onehot = onehot
         self.batch_size = batch_size
+
+        self.counter = -1
+        self.num_steps = int(
+            np.floor(len(self.extractor_indexes) / self.batch_size))
 
         self.shuffle = shuffle
         self.on_epoch_end()
@@ -36,7 +41,7 @@ class DogDataGenerator(Sequence):
         """
         :return: the number of steps ( 전체 데이터셋 수 // 배치 크기 )
         """
-        return int(np.floor(len(self.extractor_indexes) / self.batch_size))
+        return self.num_steps
 
     def __getitem__(self, index):
         """
@@ -46,7 +51,8 @@ class DogDataGenerator(Sequence):
         :return:
         """
         # Generate indexes of the batch
-        indexes = self.indexes[index * self.batch_size:(index + 1) * self.batch_size]
+        indexes = self.indexes[index *
+                               self.batch_size:(index + 1) * self.batch_size]
         batch_indexes = [self.extractor_indexes[k] for k in indexes]
 
         # Generate data
@@ -65,19 +71,26 @@ class DogDataGenerator(Sequence):
         if self.shuffle:
             np.random.shuffle(self.indexes)
 
-    def random_next_batch(self, onehot=True):
+    def next_batch(self):
         """
         batch size 을 주면 random으로 해당 batch size 만큼의 images, labels을 넘겨 줍니다.
 
         :return:
         """
-        indices = random.sample(list(self.extractor_indexes), self.batch_size)
-        batch_xs , batch_ys= self.extractor[indices]
+        if self.counter >= self.num_steps:
+            self.on_epoch_end()
+            self.counter = 0
+        else:
+            self.counter += 1
+
+        indexes = self.indexes[self.counter * self.batch_size:
+                               (self.counter + 1) * self.batch_size]
+        batch_xs, batch_ys = self.extractor[indexes]
         batch_xs = self.pipeline.transform(batch_xs)
 
-        if onehot:
-            batch_ys = map(lambda y : self.extractor.label2onehot[y], batch_ys)
-            batch_ys = list(batch_ys)
+        if self.onehot:
+            batch_ys = self.extractor.convert_to_onehot(batch_ys)
+        else:
+            batch_ys = self.extractor.convert_to_code(batch_ys)
 
-        return batch_xs ,batch_ys
-
+        return batch_xs, batch_ys
