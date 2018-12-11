1cb656c9b2735910091bbbe6bbd708cc1a32d7f5
kimseongjung <plznw4me@naver.com>
2018-12-11 16:16:19 +0900

    test code 추가
    test code 에는
    
    - Training
    - Eval
    - Restore 기능
    - Tensorboard 시각화 기능
    
    등을 확인합니다

--- lesson4/Test/Test_Tf_Simple_Alexnet.py
+++ lesson4/Test/Test_Tf_Simple_Alexnet.py
@@ -22,7 +22,7 @@ class TestTFSimpleAlexnet(unittest.TestCase):
 
     def test_alexnet(self):
 
-        # Training
+        # Define input placeholder Node
         tf_simple_alexnet.alexnet([None,32,32,3],10)
         x = tf.get_default_graph().get_tensor_by_name('x:0')
         y = tf.get_default_graph().get_tensor_by_name('y:0')
@@ -33,13 +33,14 @@ class TestTFSimpleAlexnet(unittest.TestCase):
         # Train Filewriter
         writer = tf.summary.FileWriter('tmp_log')
 
+        # Define Operation
         cost_op = tf.get_default_graph().get_tensor_by_name('cost_op:0')
         acc_op = tf.get_default_graph().get_tensor_by_name('acc_op:0')
         # acc_op = tf.get_collection(tf.GraphKeys.METRIC_VARIABLES)[0]
         train_op = tf.get_collection(tf.GraphKeys.TRAIN_OP)[0]
         merge_op = tf.summary.merge_all()
 
-        #
+        # Create Session
         sess = tf.Session()
         sess.run(tf.global_variables_initializer())
         sess.run(tf.local_variables_initializer())
@@ -47,11 +48,45 @@ class TestTFSimpleAlexnet(unittest.TestCase):
         feed_dict = {x: self.train_imgs[:60], y: self.train_labs[:60], phase_train: True, learning_rate: 0.01,
                      keep_prob: 0.5}
 
-        for i in range(5):
+        # Training
+        for step in range(5):
             _, cost, acc, summary_merge = sess.run(fetches=fetches, feed_dict=feed_dict)
-            writer.add_summary(summary_merge, global_step=i)
-            print('cost : {} , acc : {}'.format(cost, acc))
+            writer.add_summary(summary_merge, global_step=step)
+            print('step : {} cost : {} , acc : {}'.format(step, cost, acc))
 
+        # Validation
+        fetches = [cost_op, acc_op]
+        feed_dict = {x: self.val_imgs[:60], y: self.val_labs[:60], phase_train: False, learning_rate: 0.01,
+                     keep_prob: 0.5}
+        val_cost, val_acc = sess.run(fetches=fetches, feed_dict=feed_dict)
+
+        # Model save and restore
+        saver = tf.train.Saver()
+        saver.save(sess, 'tmp_dir/models/model')
+
+        # Reset graph
+        tf.reset_default_graph()
+        tf.train.import_meta_graph('./tmp_dir/models/model.meta')
+        saver = tf.train.Saver()
+        sess = tf.Session()
+        saver.restore(sess , './tmp_dir/models/model')
+
+        # Restore Node
+        x = tf.get_default_graph().get_tensor_by_name('x:0')
+        y = tf.get_default_graph().get_tensor_by_name('y:0')
+        phase_train = tf.get_default_graph().get_tensor_by_name('phase_train:0')
+        keep_prob = tf.get_default_graph().get_tensor_by_name('keep_prob:0')
+        cost_op = tf.get_default_graph().get_tensor_by_name('cost_op:0')
+        acc_op = tf.get_default_graph().get_tensor_by_name('acc_op:0')
+        fetches = [acc_op, cost_op]
+
+        # Run Session
+        feed_dict = {x: self.val_imgs[:60], y: self.val_labs[:60], phase_train: False, keep_prob: 0.5}
+        restore_acc, restore_cost = sess.run(fetches=fetches, feed_dict=feed_dict)
+
+        # Restore 했을때 Validation 결과값이 동일해야 합니다
+        self.assertEqual(float(restore_acc), float(val_acc))
+        self.assertEqual(float(restore_cost), float(val_cost))
 
 
 
