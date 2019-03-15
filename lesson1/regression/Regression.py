9306dcd2fad9516613db1622c76f391be2e455c5
kimseongjung <plznw4me@naver.com>
2019-03-15 13:16:44 +0900

    refactoring

--- lesson1/regression/Regression.py
+++ lesson1/regression/Regression.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 
 x1_data = [1, 0, 3, 0, 5]
 x2_data = [0, 2, 0, 4, 0]
-y_data  = [1, 2, 3, 4, 5]
+y_data = [1, 2, 3, 4, 5]
 
 W1 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 W2 = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
-b  = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+b = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 
 # feature 갯수만큼 곱하는 이 부분을 제외하면 one-variable과 다른 곳이 없다
 hypothesis = W1*x1_data + W2*x2_data + b
@@ -27,7 +27,7 @@ for step in range(2001):
 
     sess.run(train)
 
-    if step%20 == 0:
+    if step % 20 == 0:
         print(step, sess.run(cost), sess.run(W1), sess.run(W2), sess.run(b))
 
 sess.close()
