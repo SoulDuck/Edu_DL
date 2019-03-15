fea6775c425a62a450187e7372e3018f1998337a
kimseongjung <plznw4me@naver.com>
2019-03-15 13:45:41 +0900

    implement embedding models

---
+++ NLP/word_embedding.py
@@ -0,0 +1,216 @@
+from six.moves import urllib
+
+import errno
+import os
+import zipfile
+import numpy as np
+import tensorflow as tf
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from collections import Counter ,deque
+
+"""
+
+Text -> Vector
+단어의 유사도를 측정하기 위해 Word embedding 을 수행함.
+ Onehot vector는 너무 sparse 해져서 학습 시키기에 좋지 않음.
+
+"""
+
+WORDS_PATH = "datasets/words"
+WORDS_URL = 'http://mattmahoney.net/dc/text8.zip'
+
+
+def mkdir_p(path):
+    """Create directories, ok if they already exist.
+    This is for python 2 support. In python >=3.2, simply use:
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
+def fetch_words_data(words_url=WORDS_URL, words_path=WORDS_PATH):
+    os.makedirs(words_path, exist_ok=True)
+    zip_path = os.path.join(words_path, "words.zip")
+    if not os.path.exists(zip_path):
+        urllib.request.urlretrieve(words_url, zip_path)
+    with zipfile.ZipFile(zip_path) as f:
+        data = f.read(f.namelist()[0])
+    return data.decode("ascii").split()
+
+fetch_words_data()
+words = fetch_words_data()
+
+# Build Dictionary
+vocabulary_size = 50000
+
+vocabulary = [("UNK", None)] + Counter(words).most_common(vocabulary_size - 1)
+vocabulary = np.array([word for word, _ in vocabulary])
+dictionary = {word: code for code, word in enumerate(vocabulary)}
+data = np.array([dictionary.get(word, 0) for word in words])
+
+
+print(" ".join(words[:9]), data[:9])
+print(" ".join([vocabulary[word_index] for word_index in [5241, 3081, 12, 6, 195, 2, 3134, 46, 59]]))
+print(words[24], data[24])
+
+
+# Generate batches
+def generate_batch(batch_size, num_skips, skip_window):
+    global data_index
+    assert batch_size % num_skips == 0
+    assert num_skips <= 2 * skip_window
+    batch = np.ndarray(shape=[batch_size], dtype=np.int32)
+    labels = np.ndarray(shape=[batch_size, 1], dtype=np.int32)
+    span = 2 * skip_window + 1  # [ skip_window target skip_window ]
+    buffer = deque(maxlen=span)
+    for _ in range(span):
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    for i in range(batch_size // num_skips):
+        target = skip_window  # target label at the center of the buffer
+        targets_to_avoid = [skip_window]
+        for j in range(num_skips):
+            while target in targets_to_avoid:
+                target = np.random.randint(0, span)
+            targets_to_avoid.append(target)
+            batch[i * num_skips + j] = buffer[skip_window]
+            labels[i * num_skips + j, 0] = buffer[target]
+        buffer.append(data[data_index])
+        data_index = (data_index + 1) % len(data)
+    return batch, labels
+
+
+np.random.seed(42)
+data_index = 0
+batch, labels = generate_batch(8, 2, 1)
+batch, [vocabulary[word] for word in batch]
+labels, [vocabulary[word] for word in labels[:, 0]]
+
+# Build Model
+batch_size = 128
+embedding_size = 128  # Dimension of the embedding vector.
+skip_window = 1       # How many words to consider left and right.
+num_skips = 2         # How many times to reuse an input to generate a label.
+
+# We pick a random validation set to sample nearest neighbors. Here we limit the
+# validation samples to the words that have a low numeric ID, which by
+# construction are also the most frequent.
+valid_size = 16     # Random set of words to evaluate similarity on.
+valid_window = 100  # Only pick dev samples in the head of the distribution.
+valid_examples = np.random.choice(valid_window, valid_size, replace=False)
+num_sampled = 64    # Number of negative examples to sample.
+
+learning_rate = 0.01
+
+tf.reset_default_graph()
+
+# Input data.
+train_labels = tf.placeholder(tf.int32, shape=[batch_size, 1])
+valid_dataset = tf.constant(valid_examples, dtype=tf.int32)
+
+vocabulary_size = 50000
+embedding_size = 150
+
+# Look up embeddings for inputs.
+init_embeds = tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0)
+embeddings = tf.Variable(init_embeds)
+
+
+train_inputs = tf.placeholder(tf.int32, shape=[None])
+embed = tf.nn.embedding_lookup(embeddings, train_inputs)
+
+
+# Construct the variables for the NCE loss
+nce_weights = tf.Variable(
+    tf.truncated_normal([vocabulary_size, embedding_size],
+                        stddev=1.0 / np.sqrt(embedding_size)))
+nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
+
+# Compute the average NCE loss for the batch.
+# tf.nce_loss automatically draws a new sample of the negative labels each
+# time we evaluate the loss.
+loss = tf.reduce_mean(
+    tf.nn.nce_loss(nce_weights, nce_biases, train_labels, embed,
+                   num_sampled, vocabulary_size))
+
+# Construct the Adam optimizer
+optimizer = tf.train.AdamOptimizer(learning_rate)
+training_op = optimizer.minimize(loss)
+
+# Compute the cosine similarity between minibatch examples and all embeddings.
+norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), axis=1, keepdims=True))
+normalized_embeddings = embeddings / norm
+valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset)
+similarity = tf.matmul(valid_embeddings, normalized_embeddings, transpose_b=True)
+
+# Add variable initializer.
+init = tf.global_variables_initializer()
+
+num_steps = 10001
+
+with tf.Session() as session:
+    init.run()
+
+    average_loss = 0
+    for step in range(num_steps):
+        print("\rIteration: {}".format(step), end="\t")
+        batch_inputs, batch_labels = generate_batch(batch_size, num_skips, skip_window)
+        feed_dict = {train_inputs: batch_inputs, train_labels: batch_labels}
+
+        # We perform one update step by evaluating the training op (including it
+        # in the list of returned values for session.run()
+        _, loss_val = session.run([training_op, loss], feed_dict=feed_dict)
+        average_loss += loss_val
+
+        if step % 2000 == 0:
+            if step > 0:
+                average_loss /= 2000
+            # The average loss is an estimate of the loss over the last 2000 batches.
+            print("Average loss at step ", step, ": ", average_loss)
+            average_loss = 0
+
+        # Note that this is expensive (~20% slowdown if computed every 500 steps)
+        if step % 10000 == 0:
+            sim = similarity.eval()
+            for i in range(valid_size):
+                valid_word = vocabulary[valid_examples[i]]
+                top_k = 8 # number of nearest neighbors
+                nearest = (-sim[i, :]).argsort()[1:top_k+1]
+                log_str = "Nearest to %s:" % valid_word
+                for k in range(top_k):
+                    close_word = vocabulary[nearest[k]]
+                    log_str = "%s %s," % (log_str, close_word)
+                print(log_str)
+
+    final_embeddings = normalized_embeddings.eval()
+
+
+np.save("./my_final_embeddings.npy", final_embeddings)
+
+
+def plot_with_labels(low_dim_embs, labels):
+    assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
+    plt.figure(figsize=(18, 18))  #in inches
+    for i, label in enumerate(labels):
+        x, y = low_dim_embs[i,:]
+        plt.scatter(x, y)
+        plt.annotate(label,
+                     xy=(x, y),
+                     xytext=(5, 2),
+                     textcoords='offset points',
+                     ha='right',
+                     va='bottom')
+
+from sklearn.manifold import TSNE
+
+tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+plot_only = 500
+low_dim_embs = tsne.fit_transform(final_embeddings[:plot_only,:])
+labels = [vocabulary[i] for i in range(plot_only)]
+plot_with_labels(low_dim_embs, labels)
